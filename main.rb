74ea10b2593cbba8fc2bbfd891c1b6ff77577d8d
SeasideWhisper <benjaminhouseholder0@gmail.com>
2025-05-01 13:42:27 -0500

    COMMIT

--- main.rb
+++ main.rb
@@ -1,1 +1,32 @@
-puts 'Hello, world!'
+people = {
+    "test": {
+      dialogue: ["testDialogue1"]
+    }
+}
+affects = {
+  testDialogue1: {
+    y: {
+
+    },
+    n: {
+
+    }
+  }
+}
+
+text = {
+  testDialogue1: {
+    question: "Can I have a sandwich?",
+    y: "Yay!!!",
+    n: "Aww ):",
+    affects: affects["testDialogue1"]
+  }
+}
+
+
+
+eventArray = []
+
+def render(data)
+
+end
