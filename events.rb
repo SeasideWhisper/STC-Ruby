cf4f6071ab59ca6d383826dc83aa73ee0f36f940
SeasideWhisper <benjaminhouseholder0@gmail.com>
2025-05-05 13:41:50 -0500

    Structuring

---
+++ events.rb
@@ -0,0 +1,18 @@
+module Events
+ EVENTS = {
+    "tavern" = > {
+      name: "Party Tonight",
+      text: "There's a party at the tavern tonight, your citizens are happy!"
+      recurring: 0,
+      affects: AFFECTS["tavern"],
+      condition: { KINGDOM[:happiness] > 100 }
+    }
+ }
+
+  AFFECTS = {
+    "tavern" => {
+
+    }
+  }
+
+end
--- main.rb
+++ main.rb
@@ -4,14 +4,18 @@ people = Text::PEOPLE
 affects = Text::AFFECTS
 text = Text::TEXT
 
-
 eventArray = []
 
-money = 200
-citizens = 50
-happiness = 80
+KINGDOM = {
+    money = 200
+    citizens = 50
+    happiness = 80
+}
 
-personDialogue = people[rand(4)][:dialogue]
+
+
+personDialogue = people[rand(people.length())][:dialogue]
 puts personDialogue
 personQuestion = text[personDialogue.first][:question]
 puts personQuestion
+
